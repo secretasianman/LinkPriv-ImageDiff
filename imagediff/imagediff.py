be340d9f76da56a0af9076debc1c91552dd48e16
Kevin Su <ksu006@gmail.com>
2012-03-01 00:45:54 -0500

    refactored imagediff, implemented file size comparison

---
+++ imagediff/imagediff.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import argparse
+import os.path
+import sys
+
+from levenshtein import levenshtein_distance
+from hamming import hamming_distance
+
+def main():
+    parser = argparse.ArgumentParser(description='Calculate the percent difference between two images')
+    
+    method = parser.add_mutually_exclusive_group()
+    method.add_argument('--levenshtein', action='store_true', help='Use Levenshtein edit distance')
+    method.add_argument('--hamming', action='store_true', help='Use Hamming distance')
+    method.add_argument('--file-size', action='store_true', help='Use file size comparison')
+
+    parser.add_argument('file_name1', action='store')
+    parser.add_argument('file_name2', action='store')
+
+    arguments = parser.parse_args()
+
+    if arguments.file_size:
+        try:
+            size1 = os.path.getsize(arguments.file_name1)
+        except os.error:
+            print >> sys.stderr, "ERROR: Unable to access ", arguments.file_name1
+            sys.exit(-1)
+        try:
+            size2 = os.path.getsize(arguments.file_name2)
+        except os.error:
+            print >> sys.stderr, "ERROR: Unable to access ", arguments.file_name2
+        
+        return float(abs(size1 - size2))/max(size1, size2)
+
+    else:
+        try:
+            file1 = open(arguments.file_name1, 'r')
+            string1 = file1.read()
+        except IOError:
+            print >> sys.stderr, "ERROR: Unable to open ", arguments.file_name1
+        finally:
+            file1.close()
+
+        try:
+            file2 = open(arguments.file_name2, 'r')
+            string2 = file2.read()
+        except IOError:
+            print >> sys.stderr, "ERROR: Unable to open ", arguments.file_name2
+        finally:
+            file2.close()
+
+        if arguments.levenshtein:
+            try:
+                return float(levenshtein_distance(string1, string2))/min(len(string1), len(string2))
+            except ZeroDivisionError:
+                return 1
+        elif arguments.hamming:
+            try:
+                return float(hamming_distance(string1, string2))/min(len(string1), len(string2))
+            except ZeroDivisionError:
+                return 1
+        else:
+            print >> sys.stderr, """
+                                 ERROR: Should not have reached this point,
+                                 perhaps argparse isn't working.
+                                 """
+            sys.exit(-1)
+
+
+if __name__ == "__main__":
+    print main()
